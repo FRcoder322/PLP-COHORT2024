c7cc2a788d06fc8831f6f5ae86aa93cb423873f1
FRcoder322 <kuravalley@gmail.com>
2024-02-26 16:11:05 +0300

    user input

---
+++ python/user_input.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-module-docstring
+#  Ask the user for their name
+name = input("what is  your  name?  ")
+#ask user their age
+age = input("How old are you ?   ")
+# ask user where they live
+location =input("Where do you live ?  ")
+
+# print personalize message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
