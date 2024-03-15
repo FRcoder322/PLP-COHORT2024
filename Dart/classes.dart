9feb6846a6cc9a1f75c067bf79311a99149e2fb5
FRcoder322 <kuravalley@gmail.com>
2024-03-15 16:03:26 +0300

    add

--- Dart/classes.dart
+++ Dart/classes.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
+abstract class Displayable {
+  void display();
+}
+
 class Course {
   String Student;
   String Department;
@@ -16,12 +22,58 @@ class Course {
   void displayAdminNo() {
     print('AdminNo:$AdminNo');
   }
+}
 
-  //call the methods
-  void main() {
-    var myCourse = Course('Mary', 'Technology', '1250');
-    myCourse.displayStudent();
-    myCourse.displayDepartment();
-    myCourse.displayAdminNo();
+class CourseWithInterface extends Course implements Displayable {
+  CourseWithInterface(String student, String department, String adminNo)
+      : super(student, department, adminNo);
+
+  @override
+  void display() {
+    print('Course information:');
+    displayStudent();
+    displayDepartment();
+    displayAdminNo();
   }
+}
+
+class CourseFromFile extends Course {
+  CourseFromFile(String filePath) : super('', '', '') {
+    // Read data from file and initialize the instance
+    // For simplicity, let's assume the file contains lines in the format: Student, Department, AdminNo
+    List<String> lines = File(filePath).readAsLinesSync();
+    if (lines.isNotEmpty) {
+      List<String> data = lines[0].split(',');
+      if (data.length >= 3) {
+        Student = data[0];
+        Department = data[1];
+        AdminNo = data[2];
+      }
+    }
+  }
+}
+
+void printNumbers(int n) {
+  for (int i = 1; i <= n; i++) {
+    print(i);
+  }
+}
+
+//call the methods
+void main() {
+  var myCourse = Course('Mary', 'Technology', '1250');
+  myCourse.displayStudent();
+  myCourse.displayDepartment();
+  myCourse.displayAdminNo();
+  var derivedCourse = CourseWithInterface('John', 'Engineering', '1234');
+  derivedCourse.display();
+
+  // Demonstration of initialization with data from a file
+  var courseFromFile = CourseFromFile('data.txt');
+  courseFromFile.displayStudent();
+  courseFromFile.displayDepartment();
+  courseFromFile.displayAdminNo();
+
+  // Demonstration of loop usage
+  printNumbers(5);
 }
